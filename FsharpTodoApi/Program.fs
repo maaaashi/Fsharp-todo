45a47ceb23109f79910a45b9e031d6cce5d14e34
maaaashi <doyaseizin.7@gmail.com>
2024-08-17 23:38:14 +0900

    いらないものを消した

--- FsharpTodoApi/Program.fs
+++ FsharpTodoApi/Program.fs
@@ -1,10 +1,9 @@
-namespace FsharpTodoApi
-#nowarn "20"
 open System
 open System.Collections.Generic
 open System.IO
 open System.Linq
 open System.Threading.Tasks
+
 open Microsoft.AspNetCore
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
@@ -14,23 +13,11 @@ open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
 
-module Program =
-    let exitCode = 0
+[<EntryPoint>]
+let main args =
+    let builder = WebApplication.CreateBuilder(args)
+    let app = builder.Build()
 
-    [<EntryPoint>]
-    let main args =
+    app.Run()
 
-        let builder = WebApplication.CreateBuilder(args)
-
-        builder.Services.AddControllers()
-
-        let app = builder.Build()
-
-        app.UseHttpsRedirection()
-
-        app.UseAuthorization()
-        app.MapControllers()
-
-        app.Run()
-
-        exitCode
+    0
