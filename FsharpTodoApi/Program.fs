e4bcd36da6b16de7c46c051477608d24455eae4a
maaaashi <doyaseizin.7@gmail.com>
2024-08-18 10:18:54 +0900

    不要なimport

--- FsharpTodoApi/Program.fs
+++ FsharpTodoApi/Program.fs
@@ -1,17 +1,5 @@
-open System
-open System.Collections.Generic
-open System.IO
-open System.Linq
-open System.Threading.Tasks
-
-open Microsoft.AspNetCore
 open Microsoft.AspNetCore.Builder
-open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.HttpsPolicy
-open Microsoft.Extensions.Configuration
-open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
-open Microsoft.Extensions.Logging
 
 [<EntryPoint>]
 let main args =
