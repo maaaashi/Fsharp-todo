23af76b80878b3baa43beeacd78e6684b5596018
maaaashi <doyaseizin.7@gmail.com>
2024-09-19 22:32:07 +0900

    Blogドメインの修正

--- FsharpTodoApi/Domain/type/Blog.fs
+++ FsharpTodoApi/Domain/type/Blog.fs
@@ -1,6 +1,25 @@
 namespace FsharpTodoApi.Domain
 
+type ArticleId = ArticleId of string
+type ArticleTitle = ArticleTitle of string
+type ArticleBody = ArticleBody of string
+
 [<RequireQualifiedAccess>]
-type Blog = {
-    id: string
-}
+type Article =
+    { id: ArticleId
+      title: ArticleTitle
+      body: ArticleBody }
+
+type UserId = UserId of string
+type UserName = UserName of string
+
+[<RequireQualifiedAccess>]
+type Author =
+    { id: UserId
+      name: UserName }
+
+[<RequireQualifiedAccess>]
+type Blog = Article * Author
+
+[<RequireQualifiedAccess>]
+type Blogs = Blogs of Blog list
