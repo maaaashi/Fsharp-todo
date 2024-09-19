5bf872b4c940dea8458981433722e0c7f39d1296
maaaashi <doyaseizin.7@gmail.com>
2024-09-19 23:25:50 +0900

    エラー解消

--- FsharpTodoApi/Domain/type/Blog.fs
+++ FsharpTodoApi/Domain/type/Blog.fs
@@ -4,7 +4,6 @@ type ArticleId = ArticleId of string
 type ArticleTitle = ArticleTitle of string
 type ArticleBody = ArticleBody of string
 
-[<RequireQualifiedAccess>]
 type Article =
     { id: ArticleId
       title: ArticleTitle
@@ -13,13 +12,10 @@ type Article =
 type UserId = UserId of string
 type UserName = UserName of string
 
-[<RequireQualifiedAccess>]
 type Author =
     { id: UserId
       name: UserName }
 
-[<RequireQualifiedAccess>]
 type Blog = Article * Author
 
-[<RequireQualifiedAccess>]
 type Blogs = Blogs of Blog list
--- FsharpTodoApi/Handler/GetBlogsHandler.fs
+++ FsharpTodoApi/Handler/GetBlogsHandler.fs
@@ -1,17 +1,25 @@
 namespace FsharpTodoApi.Handler
 
 open System.Threading.Tasks
-open Microsoft.AspNetCore.Http
 open FsharpTodoApi.Domain
+
+type IResult =
+    | Ok of Blogs
+    | Error of string
 
 module GetBlogsHandler =
     let handler: Task<IResult> =
         async {
-            let blogs =
-                Blog [{
-                  id = ""
-                }]
+            let article =
+              { id = ArticleId "blog_1"
+                title = ArticleTitle "タイトル1"
+                body = ArticleBody "本文1" }
+            let author =
+                { id = UserId "user_1"
+                  name = UserName "ユーザー1" }
 
-            return Ok blogs
+            let blog = Blog (article, author)
+
+            return Ok (Blogs [blog])
         }
         |> Async.StartAsTask
